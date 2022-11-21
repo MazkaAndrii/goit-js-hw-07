4175ea6be9e4e8ae0db24e63d732fafd75132562
Mazka Andrii <andriy.mazka@gmail.com>
2022-11-21 20:01:55 +0200

    fix_hmjs7p2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkUp = galleryItems
+   .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>;`;
+   })
+   .join('');
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkUp);
+const lightbox = new SimpleLightbox('.gallery__item', {
+   captionsData: 'alt',
+   captionDelay: 250,
+});
+// console.log(galleryItems);
