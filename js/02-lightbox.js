682de92e6bc3cf94a288c84646b9e8b716162585
Mazka Andrii <andriy.mazka@gmail.com>
2022-11-21 20:06:10 +0200

    fix2_hmjs7p2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,7 +5,7 @@ const galleryItemsMarkUp = galleryItems
    .map(item => {
       return `<a class="gallery__item" href="${item.original}">
    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>;`;
+</a>`;
    })
    .join('');
 
